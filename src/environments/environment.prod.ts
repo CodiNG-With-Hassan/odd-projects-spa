7471ebf9313ffb888c9552907fa3719ccff0ae5d
Hassan Khousheish <hassan@crunchrapps.com>
2022-11-20 23:12:49 +0100

    Update Environment Model

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ import { Environment } from '@Models/environment.models';
 export const environment: Environment = {
   production: true,
   defaultLanguage: 'en',
+  apiUrl: 'http://localhost:3000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ import { Environment } from '@Models/environment.models';
 export const environment: Environment = {
   production: false,
   defaultLanguage: 'en',
+  apiUrl: 'http://localhost:3000',
 };
