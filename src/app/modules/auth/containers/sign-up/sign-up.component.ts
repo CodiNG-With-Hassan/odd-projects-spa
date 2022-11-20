1faa35dcef6a81cc585571fda1f9cd23fbdaba15
Hassan Khousheish <hassan@crunchrapps.com>
2022-11-20 23:17:01 +0100

    Implement Sign Up Page

---
+++ src/app/modules/auth/containers/sign-up/sign-up.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FieldNames } from '@Enums/fields.enum';
+import { AuthFacade } from '@Modules/auth/store/auth.facade';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'ngt-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+})
+export class SignUpComponent implements OnInit {
+  public signUpFormGroup!: FormGroup;
+  public pending$: Observable<boolean> = this.authFacade.selectAuthPending$;
+
+  public readonly fieldNames: typeof FieldNames = FieldNames;
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    public readonly authFacade: AuthFacade,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.signUpFormGroup = this.initializeFormGroup();
+  }
+
+  submitForm(): void {
+    this.authFacade.signUp(this.signUpFormGroup.value);
+  }
+
+  initializeFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      [FieldNames.FirstName]: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(16)]],
+      [FieldNames.LastName]: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(16)]],
+      [FieldNames.Username]: ['', [Validators.required]],
+      [FieldNames.Email]: ['', [Validators.required, Validators.email]],
+      [FieldNames.Password]: ['', [Validators.required]],
+    });
+  }
+}
---
+++ src/app/shared/enums/fields.enum.ts
@@ -0,0 +1,8 @@
+export enum FieldNames {
+  FirstName = 'firstName',
+  LastName = 'lastName',
+  Identity = 'identity',
+  Username = 'username',
+  Email = 'email',
+  Password = 'password',
+}
